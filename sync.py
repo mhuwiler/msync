4466690e7aa5457430f2370b1824d2e0d70d5ac6
Marc Huwiler <marc.huwiler@cern.ch>
2021-09-17 23:59:59 +0200

    Obtaining date from filename and modification time

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import glob
-import datetime
+from datetime import datetime
 
 
 devicefolder = "/Users/mhuwiler/.AFTVolumes/samsung SM-A520F/storage/3565-3062/DCIM/Camera/"
@@ -15,7 +15,23 @@ for file in os.listdir(devicefolder):
 	if (os.path.isfile(devicefolder+file)): 
 		#print file
 
+		# Based on modification time
 		date = os.path.getmtime(devicefolder+file)
 
 		print "File {} modified at {}".format(file, date)
+
+		foldername = datetime.fromtimestamp(date).strftime("%Y_%m_%d")
+
+		print "Folder: {}".format(foldername)
+
+		# Based on filename
+		framedate = file.split("_")[0]
+
+		print framedate
+
+		stamp = datetime.strptime(framedate, "%Y%m%d")
+
+		print "Name: {}".format(stamp.strftime("%Y_%m_%d"))
+
+		
 
