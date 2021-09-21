bb61479ab37418eb2b099fdf23216b4912fc8be8
Marc Huwiler <marc.huwiler@cern.ch>
2021-09-21 23:23:20 +0200

    Changed checksum to sha256 and added functions

--- sync.py
+++ sync.py
@@ -6,11 +6,30 @@ import glob
 from datetime import datetime
 import shutil
 import hashlib
+import json
 
 
 devicefolder = "/Users/mhuwiler/.AFTVolumes/samsung SM-A520F/storage/3565-3062/DCIM/Camera/"
 
 localfolder = "/Users/mhuwiler/Pictures/Camera_Phone/"
+
+
+def getmd5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def getsha256(fname):
+    hash_sha256 = hashlib.sha256()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
+
+
+bookkeeping = {}
 
 
 for file in sorted(filter(os.path.isfile, [devicefolder+f for f in os.listdir(devicefolder)]), key=os.path.getmtime): #Getting files sorted by date
@@ -39,9 +58,15 @@ for file in sorted(filter(os.path.isfile, [devicefolder+f for f in os.listdir(de
 
 		print "Name: {}".format(foldername)
 
-		checksum = hashlib.md5(open(devicefolder+file, "rb").read()).hexdigest()
+		checksum = hashlib.sha256(open(devicefolder+file, "rb").read()).hexdigest()
+
+		print getsha256(devicefolder+file)
 
 		print checksum
+
+		bookkeeping.update({file:checksum})
+
+
 
 
 		currentdest = localfolder+foldername
